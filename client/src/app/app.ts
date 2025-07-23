199998e0784368d1462caab97b2af5486357461c
rgerero <31179503+rgerero@users.noreply.github.com>
2025-07-23 09:06:41 +0800

    show how to use toPromise for single-value operations

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private http = inject(HttpClient);
   protected readonly title = signal('client');
+  protected members = signal<any>([]);
+  
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch (error) {
+      console.log('Error fetching members:', error);
+      return [];
+    }
+  }
 }
