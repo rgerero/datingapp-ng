287af1ec1690e1b4f1a5d29146c0bbfeb4cd96c0
rgerero <31179503+rgerero@users.noreply.github.com>
2025-08-02 07:08:14 +0800

    create DTOs to pass data from 1 layer to another

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,23 +1,30 @@
 using System.ComponentModel;
 using System.Security.Cryptography;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AccountController(AppDbContext context) : BaseApiController
     {
         [HttpPost("register")]  //api/account/register
-        public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await EmailExists(registerDto.Email))
+            {
+                return BadRequest("Email is already in use");
+            }
+
             var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                Email = email,
-                DisplayName = displayName,
-                Passwordhash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                Email = registerDto.Email,
+                DisplayName = registerDto.DisplayName,
+                Passwordhash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -25,6 +32,11 @@ namespace API.Controllers
             await context.SaveChangesAsync();
 
             return user;
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
         }
     }
 }
