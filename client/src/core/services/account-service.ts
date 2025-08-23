b01f75210a58633497920b2a0d37d5b2bea655bd
rgerero <31179503+rgerero@users.noreply.github.com>
2025-08-23 09:26:03 +0800

    implement login and make it persist using signals and localstorage

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../models/user';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +9,21 @@ import { inject, Injectable } from '@angular/core';
 export class AccountService {
   private http = inject(HttpClient);
   private baseUrl = 'https://localhost:5001/api';
-  
+  currentUser = signal<User | null>(null);
+
   login(creds: any) {
-    return this.http.post(`${this.baseUrl}/account/login`, creds)
+    return this.http.post<User>(`${this.baseUrl}/account/login`, creds).pipe(
+      tap(user => {
+        if (user) {
+          this.currentUser.set(user);
+          localStorage.setItem('user', JSON.stringify(user));
+        }
+      })
+    );
+  }
+
+  logout() {
+    this.currentUser.set(null);
+    localStorage.removeItem('user');
   }
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 
@@ -11,16 +11,26 @@ import { AccountService } from '../../core/services/account-service';
 export class Nav {
   private accountService = inject(AccountService);
   protected creds: any = {};
+  protected loggedIn = signal(false);
 
   login() {
     console.log('Logging in with credentials:', this.creds);
     this.accountService.login(this.creds).subscribe({
       next: (response) => {
+        this.loggedIn.set(true);
         console.log('Login successful', response);
+        this.creds = {}; // Clear credentials after successful login
       },
       error: (error) => {
+        alert(error.message);
         console.error('Login failed', error);
       }
     });
+  }
+
+  logout() {
+    this.loggedIn.set(false);
+    this.accountService.logout();
+    this.creds = {}; // Clear credentials on logout
   }
 }
