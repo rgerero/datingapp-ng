8b1eafbebda9183dabdc5adf7283b0c8e2482c17
rgerero <31179503+rgerero@users.noreply.github.com>
2025-08-25 07:15:59 +0800

    Implement toat using daisy UI

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
@@ -11,18 +12,21 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  protected toast = inject(ToastService);
   protected router = inject(Router);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (response) => {
-        console.log('Login successful', response);
+        this.toast.success('Login successfully');
         this.router.navigateByUrl('/members');
+        console.log('Login successful', response);
         this.creds = {}; // Clear credentials after successful login
       },
       error: (error) => {
-        alert(error.message);
+        this.toast.error(error.error);
+        console.error(error);
       }
     });
   }
