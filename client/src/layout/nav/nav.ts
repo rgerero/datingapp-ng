0d372a2e773ef096591f815d43db9e5b7391cb82
rgerero <31179503+rgerero@users.noreply.github.com>
2025-08-25 06:48:43 +0800

    Implement navigation links

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,21 +1,24 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  protected router = inject(Router);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (response) => {
         console.log('Login successful', response);
+        this.router.navigateByUrl('/members');
         this.creds = {}; // Clear credentials after successful login
       },
       error: (error) => {
@@ -26,5 +29,6 @@ export class Nav {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
