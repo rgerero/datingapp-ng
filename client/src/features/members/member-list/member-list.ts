24a269d5b8d5ae5db26ea26735e9c8ec1a5b2681
rgerero <31179503+rgerero@users.noreply.github.com>
2025-10-04 09:12:04 +0800

    create and style member card

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -3,10 +3,11 @@ import { MemberService } from '../../../core/services/member-service';
 import { Observable } from 'rxjs';
 import { Member } from '../../../models/member';
 import { AsyncPipe } from '@angular/common';
+import { MemberCard } from "../member-card/member-card";
 
 @Component({
   selector: 'app-member-list',
-  imports: [AsyncPipe], //this will handle the subscription and unsubscription automatically
+  imports: [AsyncPipe, MemberCard], //this will handle the subscription and unsubscription automatically
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
