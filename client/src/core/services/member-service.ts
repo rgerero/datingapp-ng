c9123c33a979df4c476f103d814595cab3fdb8f8
rgerero <31179503+rgerero@users.noreply.github.com>
2025-10-04 09:26:12 +0800

    implement interceptor for jwt token

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -2,29 +2,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Member } from '../../models/member';
-import { AccountService } from './account-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   private baseUrl = environment.apiUrl;
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'members');
   }
 
   getMember(id: string) {
-    return this.http.get<Member>(this.baseUrl + 'members/' + id, this.getHttpOptions());
-  }
-
-  private getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + this.accountService.currentUser()?.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
   }
 }
